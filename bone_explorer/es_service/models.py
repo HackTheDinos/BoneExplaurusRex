c9557630ee440a5a8c5cc338435cb40a4e9177e5
Michael Perez <michael.perez@chatid.com>
2015-11-22 06:24:38 -0500

    add suggest fields to es models; complete ingestion service

--- bone_explorer/es_service/models.py
+++ bone_explorer/es_service/models.py
@@ -6,14 +6,27 @@
     Elastic Search Models
 """
 
-from elasticsearch_dsl import DocType, String, Date
+from elasticsearch_dsl import DocType, String, Completion
 
 
-class Document(DocType):
+class Scan(DocType):
     genus = String()
     species = String()
-    date_scanned = Date()
-    filename = String()
+    s3uri = String()
+    genus_suggest = Completion(
+        analyzer='simple',
+        payloads=True,
+        perserve_seperators=True,
+        perserve_position_increments=True,
+        max_input_length=50
+    )
+    species_suggest = Completion(
+        analyzer='simple',
+        payloads=True,
+        perserve_seperators=True,
+        perserve_position_increments=True,
+        max_input_length=50
+    )
 
     class Meta:
         index = 'ct_scans'
