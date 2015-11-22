ecd87da796349a90245e5871a54b41f75ea3d788
Joseph Spens <joseph@workmarket.com>
2015-11-22 10:10:37 -0500

    Add UI for search results

---
+++ bone_explorer/webapp/app/search/main.js
@@ -0,0 +1,14 @@
+var selectModule = require('./select.js');
+var buttonModule = require('./button.js');
+require('./main.scss');
+
+module.exports = function () {
+    var main = document.querySelector('.main');
+    var genusSelect = selectModule(main, 'genus');
+    var speciesSelect = selectModule(main, 'species');
+    var results = document.createElement('section');
+    results.classList.add('search--results');
+    buttonModule(main, [genusSelect, speciesSelect]);
+    main.appendChild(results);
+    var resultsView = require('./views/results.js');
+};
