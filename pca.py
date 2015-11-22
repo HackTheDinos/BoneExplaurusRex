58ddd771bc73a9f77817113fb6d4a2efbb72e591
jwillage <jwillage@gmail.com>
2015-11-22 06:30:47 -0500

    pca.py and pca files

---
+++ pca.py
@@ -0,0 +1,27 @@
+import configparser
+config = configparser.ConfigParser()
+
+config.read('data/Alligator_Hatchling_1_.pca')
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+    
+averaging = ConfigSectionMap('CalibValue')['averaging']
+skip = ConfigSectionMap('CalibValue')['skip']
+timingVal = ConfigSectionMap('Detector')['timingval']
+current = ConfigSectionMap('Xray')['current']
+
+print(averaging)
+print(skip)
+print(timingVal)
+print(current)
